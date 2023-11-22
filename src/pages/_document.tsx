9eb5dd86eb6a53aa0edfdc4b876577c6104c2a37
jusdy <pauleta0126@gmail.com>
2023-11-22 10:28:49 +0900

    feat: add google tag manager script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,27 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
+
+const GoogleAnalyticsScript = () => {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-DVJG6CWTNM"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-DVJG6CWTNM');`}
+      </Script>
+    </>
+  );
+};
 
 export default function Document() {
   return (
     <Html lang="en">
+      <GoogleAnalyticsScript />
       <Head />
       <body>
         <Main />
