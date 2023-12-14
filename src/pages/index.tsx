9322d6fd8336ccdea81ccb2d5e8c35c93644bda9
jusdy <pauleta0126@gmail.com>
2023-12-14 13:33:50 +0900

    fix: comment out the pools section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home() {
           <TokenCards />
           <SwapExplain />
           <BridgeExplain />
-          <PoolExplain />
+          {/* <PoolExplain /> */}
           <StartButtonGroup />
         </Stack>
       </Box>
